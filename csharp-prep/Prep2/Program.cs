32e6bfbc2e627c7f8395e320e32ba2b14692e5e7
corysmith97 <corysmith7991@gmail.com>
2023-01-06 16:12:41 -0700

    Prep Assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade this semeser? ");
+        string strGrade = Console.ReadLine();
+        int grade;
+        grade = Convert.ToInt32(strGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade was an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade was a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade was a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade was a D");
+        }
+        else if (grade <= 59)
+        {
+            Console.WriteLine("Your grade was a F");
+        }
     }
 }
