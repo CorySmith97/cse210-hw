5d0eccb003870a6dc1d80f87e3ea45584c511855
corysmith97 <corysmith7991@gmail.com>
2023-01-09 10:43:37 -0700

    Prep3 Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int userNumberInt = 14;
+
+        while (userNumberInt != number)
+        {
+            string userNumber = Console.ReadLine();
+            userNumberInt = Convert.ToInt32(userNumber);
+            if (userNumberInt == number)
+            {
+                Console.WriteLine("You got it!");
+            }
+
+            else if (userNumberInt >= number)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            else if (userNumberInt <= number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
