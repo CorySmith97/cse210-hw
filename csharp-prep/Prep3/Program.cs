8676d13624bc2c9cd48669773dd776cf902347c8
corysmith97 <corysmith7991@gmail.com>
2023-01-09 10:43:52 -0700

    Prep3 Finalized

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,15 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number? ");
-        
+        Console.WriteLine("A random number has been generated. Take a guess!");
 
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
-        int userNumberInt = 14;
+        int number = randomGenerator.Next(1, 100);
+        int userNumberInt = 0;
 
         while (userNumberInt != number)
         {
+            Console.WriteLine("What is your guess?");
+
             string userNumber = Console.ReadLine();
             userNumberInt = Convert.ToInt32(userNumber);
             if (userNumberInt == number)
@@ -24,7 +25,7 @@ class Program
             {
                 Console.WriteLine("Lower");
             }
-            
+
             else if (userNumberInt <= number)
             {
                 Console.WriteLine("Higher");
